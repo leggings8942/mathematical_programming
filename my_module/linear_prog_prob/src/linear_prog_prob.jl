c0462cb70d94123588bcc3ffa59f53274299e8ea
leggings8942 <h_nagino@adinte.co.jp>
2024-12-25 13:23:36 +0900

    単体法の実装完了

--- my_module/linear_prog_prob/src/linear_prog_prob.jl
+++ my_module/linear_prog_prob/src/linear_prog_prob.jl
@@ -3,36 +3,68 @@ export simplex
 
 function simplex(object::Matrix{T}, conditions_A::Matrix{T}, conditions_b::Vector{T}) where T <: Union{Int, AbstractFloat}
     @assert size(object)[1] == 1 "the objective variable must be a horizontal vector :: size(object) = $(size(object))"
-    @assert size(conditions_A)[1] == length(conditions_b) "the conditions variable's dimentions do not match :: size(conditions_A) = $(size(conditions_A)), length(conditions_b) = $(length(conditions_A))"
+    @assert size(conditions_A)[1] == length(conditions_b) "the conditions variable's dimentions do not match :: size(conditions_A) = $(size(conditions_A)), length(conditions_b) = $(length(conditions_b))"
     @assert size(object)[2] == size(conditions_A)[2] "the conditions variable's dimentions do not match :: size(object) = $(size(object)), size(conditions_A) = $(size(conditions_A))"
 
     condits, expvars = size(conditions_A)
     @assert condits < expvars "The number of explanatory variables is greater than the number of conditions."
 
-    N   = conditions_A[1:end, 1:(expvars - condits)]
-    B   = conditions_A[1:end, (expvars - condits + 1):end]
-    C_N = object[1, 1:(expvars - condits)]
-    C_B = object[1, (expvars - condits + 1):end]
-    
-    X_B = B * conditions_b
+    nonBasicSet = Set(1:(expvars - condits))
+    basicSet    = Set((expvars - condits + 1):expvars)
+    x           = zeros(Float64, expvars)
+    z           = 0.0
+    count = 0
+    while true
+        nonBasicSpecifier = collect(nonBasicSet)
+        basicSpecifier    = collect(basicSet)
 
-    b_dash   = X_B
-    C_N_dash = C_N' - C_B' * inv(B) * N
-    N_dash   = inv(B) * N
+        nonBasic = conditions_A[1:end, nonBasicSpecifier]
+        basic    = conditions_A[1:end, basicSpecifier]
+        coefNonBasic = object[1, nonBasicSpecifier]
+        coefBasic    = object[1, basicSpecifier]
 
-    z = C_B' * b_dash
+        inv_b  = inv(basic)
+        B_dash = inv_b * conditions_b
+        N_dash = inv_b * nonBasic
 
-    display(Text("object ="))
-    display(object)
-    display(Text("conditions_A ="))
-    display(conditions_A)
-    display(Text("conditions_b ="))
-    display(conditions_b)
-    # @show N B C_N C_B
-    # @show b_dash C_N_dash N_dash
-    # @show z
+        xBasic = B_dash
+        expect = (coefNonBasic' - coefBasic' * N_dash)'
 
-    return X_B
+        if count ≠ 0 && count % 1e5 == 0
+            println(count, "回目の反復試行")
+        end
+
+        if maximum(expect) <= 0
+            println("収束しました")
+            println("結果：", count, "回の反復試行を行いました")
+            x[basicSpecifier] = xBasic
+            z = coefBasic' * xBasic
+            break
+        end
+
+        idx_nonBasic = argmax(expect)
+        xBasic2      = xBasic ./ N_dash[:, idx_nonBasic]
+        xBasic2[xBasic2 .< 0] .= Inf64 # 非負制約
+        idx_basic    = argmin(xBasic2)
+
+        if all(isinf.(xBasic2))
+            println("収束に失敗しました")
+            println("結果：", count, "回の反復試行を行いました")
+            x[basicSpecifier] = xBasic
+            z = coefBasic' * xBasic
+            break
+        end
+
+        push!(nonBasicSet, basicSpecifier[idx_basic])
+        pop!( nonBasicSet, nonBasicSpecifier[idx_nonBasic])
+        push!(basicSet,    nonBasicSpecifier[idx_nonBasic])
+        pop!( basicSet,    basicSpecifier[idx_basic])
+
+        # 反復回数のカウント
+        count += 1
+    end
+
+    return x, z
 end
 
 end # module linear_prog_prob
